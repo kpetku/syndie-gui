7100f2acc3d79fad7015152627c43c036beda782
Keith Petkus <keith@keithp.net>
2020-07-19 22:24:31 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"os/user"
+)
+
+func main() {
+	client := NewGUI()
+
+	usr, err := user.Current()
+	if err != nil {
+		panic("Error obtaining current user")
+	}
+	client.Start(usr.HomeDir + "/.syndie/db/bolt.db")
+}
